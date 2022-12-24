2b7d9fa9c86a6956ca85a3a05e087851ba3d39ee
Clifford Frempong <cliffordfrempong@Cliffords-MacBook-Pro.local>
2022-12-24 20:19:44 +0000

    made changes to dag

--- airflow/dags/spark_etl.py
+++ airflow/dags/spark_etl.py
@@ -1,10 +1,23 @@
+"""
+This DAG will trigger a job in a Zeppelin notebook with using an HTTP POST request every hour.
+You'll need to create a connection to the Zeppelin server in the Airflow UI using the zeppelin_conn connection ID.
+You can do this by going to the Admin > Connections page in the Airflow UI and adding a new connection with the following details:
+* Conn Id: zeppelin_conn
+* Conn Type: HTTP
+* Host: http://localhost:8085
+You'll also need to make sure that the Airflow webserver has access to the Zeppelin server.
+You can do this by adding the IP address of the Airflow webserver to the zeppelin.server.allowed.origins property in the Zeppelin configuration.
+Once you've set up the connection and the DAG, you can trigger the DAG using the Airflow UI or the airflow command-line tool.
+The DAG will then run on the schedule you defined, triggering the Zeppelin job at the specified intervals.
+"""
+
 from airflow import DAG
 from airflow.operators.http_operator import SimpleHttpOperator
 from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'me',
-    'start_date': datetime(2022, 12, 30),
+    'start_date': datetime(2022, 12, 10),
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
